568801df40f92712e1d11d28b61506c7f0e1ae46
MiniNaim <mininaim@gmail.com>
2013-12-19 12:46:49 +0000

    Change config.inc.php information

--- config.inc.php
+++ config.inc.php
@@ -10,8 +10,8 @@ define('DB_NAME', 'student');
 define('app_title', 'Student App'); /** Web site title */
 define('app_description', 'A general student-subject system'); 
 
-define('app_url', 'http://localhost/student/app/'); /** change the url when you upload the files to the server */
-define('app_email', 'ernest@souqalmal.com'); /** to get notifications .. etc */
+define('app_url', 'http://localhost/student/'); /** change the url when you upload the files to the server */
+define('app_email', 'mininaim@gmail.com'); /** to get notifications .. etc */
 
-define('app_owner_url', 'http://mininaim.com/app/student/');
+define('app_owner_url', 'http://localhost/student/');
 define('app_dashboard_path', 'dashboard/'); /** don't change this unless you are a developer  :) */
